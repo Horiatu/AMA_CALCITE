a70250b161cb90b6f956911b4682df4e19e61bab
Horia Tudosie <htudosie@esri.ca>
2024-02-13 20:42:19 -0500

    Calcite Action Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import {useState} from "react";
-// import reactLogo from "./assets/react.svg"
-// import viteLogo from "./vite.svg";
 import "./App.css";
+
+import {setAssetPath} from "@esri/calcite-components/dist/components";
+setAssetPath(window.location.href);
+
+import {CalciteShell, CalciteShellPanel, CalciteActionBar, CalciteAction} from "@esri/calcite-components-react";
 
 function App() {
 	const [count] = useState(0);
@@ -11,30 +14,64 @@ function App() {
 
 	return (
 		<>
-			<h1>
-				AMA C a l c i t e
-				<div id="rightBtns">
-					<a
-						href="https://vitejs.dev"
-						target="_blank">
-						<img
-							src={viteLogo}
-							className="logo"
-							alt="Vite logo"
-						/>
-					</a>
-					<a
-						href="https://react.dev"
-						target="_blank">
-						<img
-							src={reactLogo}
-							className="logo react"
-							alt="React logo"
-						/>
-					</a>
-				</div>
-			</h1>
-			<div id="viewDiv"></div>
+			<CalciteShell content-behind>
+				<h1
+					id="header-title"
+					slot="header">
+					AMA C a l c i t e
+					<div id="rightBtns">
+						<a
+							href="https://vitejs.dev"
+							target="_blank">
+							<img
+								src={viteLogo}
+								className="logo"
+								alt="Vite logo"
+							/>
+						</a>
+						<a
+							href="https://react.dev"
+							target="_blank">
+							<img
+								src={reactLogo}
+								className="logo react"
+								alt="React logo"
+							/>
+						</a>
+					</div>
+				</h1>
+				<CalciteActionBar slot="action-bar">
+					<CalciteAction
+						data-action-id="layers"
+						icon="layers"
+						text="Layers"></CalciteAction>
+					<CalciteAction
+						data-action-id="basemaps"
+						icon="basemap"
+						text="Basemaps"></CalciteAction>
+					<CalciteAction
+						data-action-id="legend"
+						icon="legend"
+						text="Legend"></CalciteAction>
+					<CalciteAction
+						data-action-id="bookmarks"
+						icon="bookmark"
+						text="Bookmarks"></CalciteAction>
+					<CalciteAction
+						data-action-id="print"
+						icon="print"
+						text="Print"></CalciteAction>
+					<CalciteAction
+						data-action-id="information"
+						icon="information"
+						text="Information"></CalciteAction>
+				</CalciteActionBar>
+				<CalciteShellPanel
+					slot="panel-start"
+					display-mode="float"></CalciteShellPanel>
+
+				<div id="viewDiv"></div>
+			</CalciteShell>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
+import {setAssetPath} from "@esri/calcite-components/dist/components";
+setAssetPath(window.location.href);
 
 ReactDOM.createRoot(document.getElementById("root") as any).render(
 	<React.StrictMode>
