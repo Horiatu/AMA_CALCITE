c2cd59fa783bcf1a1079de55875de89d978ae1e3
Horia Tudosie <htudosie@esri.ca>
2024-03-13 16:15:58 -0400

    no app profiler

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 
-const onRender = (id, phase, actualDuration, baseDuration, startTime, commitTime) => {
-	console.log(
-		`Profiler: id=${id}, phase=${phase}, actualDuration=${actualDuration}, baseDuration=${baseDuration}, startTime=${startTime}, commitTime=${commitTime}`
-	);
-};
+// const onRender = (id, phase, actualDuration, baseDuration, startTime, commitTime) => {
+// 	console.log(
+// 		`Profiler: id=${id}, phase=${phase}, actualDuration=${actualDuration}, baseDuration=${baseDuration}, startTime=${startTime}, commitTime=${commitTime}`
+// 	);
+// };
 
 ReactDOM.createRoot(document.getElementById("root") as any).render(
-	<React.StrictMode>
-		<React.Profiler
-			id="App"
-			onRender={onRender}>
-			<App />
-		</React.Profiler>
-	</React.StrictMode>
+	// <React.StrictMode>
+	// 	<React.Profiler
+	// 		id="App"
+	// 		onRender={onRender}>
+	<App />
+	// 	</React.Profiler>
+	// </React.StrictMode>
 );
